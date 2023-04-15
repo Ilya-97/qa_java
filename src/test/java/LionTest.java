2fadafc69fbe756dc1fd6f4584e03ff22e2bb3b1
Ilya-Peshkov <ilapeskov97@gmail.com>
2023-04-15 17:33:29 +0300

    First_commit

---
+++ src/test/java/LionTest.java
@@ -0,0 +1,69 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class LionTest {
+    private final String sex;
+    private final String animalKind;
+    private final boolean isManeResult;
+
+
+    public LionTest(String sex, String animalKind, boolean isManeResult){
+        this.sex = sex;
+        this.animalKind = animalKind;
+        this.isManeResult = isManeResult;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] testParams() {
+        return new Object[][]{
+                {"Самец", "Хищник", true},
+                {"Самка", "Травоядное", false},
+                {"Непонятно", "Травоядное", false},
+        };
+    }
+
+
+    @Before
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Mock
+    Feline feline;
+
+
+    @Test
+    public void lionManeGenderMatchingTest() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        assertEquals(lion.doesHaveMane(), isManeResult);
+    }
+
+    @Test
+    public void testLionFoodType() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        Mockito.when(feline.getFood("Хищник")).thenReturn(Collections.singletonList(animalKind));
+        assertEquals((lion.getFood()), Collections.singletonList(animalKind));
+    }
+
+    @Test
+    public void getKittensMethodTestLion() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(lion.getKittens()).thenReturn(1);
+        int actual = lion.getKittens();
+        assertEquals(1, actual);
+    }
+
+}
